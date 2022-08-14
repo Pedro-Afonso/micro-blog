8215423e126ceb64832af6d8574e6adeb206591c
Pedro-Afonso <pedro.766@hotmail.com>
2022-08-14 10:26:35 -0300

    feat: Add AuthContext Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
 import { Navbar } from "./shared/components/Navbar";
+import { AppAuthProvider } from "./shared/context/AuthContext";
+import { useAuthentication } from "./shared/hooks/useAuthentication";
 
 export const App = () => {
+  const { auth } = useAuthentication();
   return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRoutes />
-    </BrowserRouter>
+    <AppAuthProvider auth={auth}>
+      <BrowserRouter>
+        <Navbar />
+        <AppRoutes />
+      </BrowserRouter>
+    </AppAuthProvider>
   );
 };
