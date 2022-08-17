e7a3c51d7fa989f6741b859089cc9641ac8ba844
Pedro-Afonso <pedro.766@hotmail.com>
2022-08-17 11:45:39 -0300

    fead: Add isLoading parameter

--- src/shared/components/FlashMessage/index.tsx
+++ src/shared/components/FlashMessage/index.tsx
@@ -1,22 +1,25 @@
-import { card } from "./styles.module.css";
+import { card, success, danger, loading } from "./styles.module.css";
 
 interface IFlashMessageProps {
   error?: string | null;
   message?: string | null;
+  isLoading?: boolean;
 }
 
 export const FlashMessage: React.FC<IFlashMessageProps> = ({
   error,
   message,
+  isLoading,
 }) => {
-  if (!message && !error) {
+  if (!message && !error && !isLoading) {
     return <></>;
   }
 
   return (
     <div className={card}>
-      {message && <p>{message}</p>}
-      {error && <p>{error}</p>}
+      {message && <p className={success}>{message}</p>}
+      {error && <p className={danger}>{error}</p>}
+      {isLoading && <p className={loading}>Carregando...</p>}
     </div>
   );
 };
