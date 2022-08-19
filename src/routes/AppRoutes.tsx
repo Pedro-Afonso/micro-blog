a9778d700cb1a1ee0a16ab3e614ca78c34ff390b
Pedro-Afonso <pedro.766@hotmail.com>
2022-08-19 10:34:42 -0300

    feat: Add protection to routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { CreatePost } from "../pages/CreatePost";
 import { Dashboard } from "../pages/Dashboard";
 import { EditPost } from "../pages/EditPost";
@@ -6,17 +7,42 @@ import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Post } from "../pages/Post";
 import { Register } from "../pages/Register";
+import { useAuthContext } from "../shared/context/AuthContext";
+import { useAuthentication } from "../shared/hooks/useAuthentication";
 
 export const AppRoutes = () => {
+  const { user } = useAuthContext();
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  if (loadingUser) {
+    console.log("loading");
+    console.log(user);
+    return <p>Carregando...</p>;
+  }
+
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/post/create" element={<CreatePost />} />
-      <Route path="/post/edit/:id" element={<EditPost />} />
+      <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+      <Route
+        path="/register"
+        element={!user ? <Register /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/post/create"
+        element={user ? <CreatePost /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/post/edit/:id"
+        element={user ? <EditPost /> : <Navigate to="/login" />}
+      />
       <Route path="/post/:id" element={<Post />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/home" element={<Home />} />
+      <Route
+        path="/dashboard"
+        element={user ? <Dashboard /> : <Navigate to="/login" />}
+      />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
